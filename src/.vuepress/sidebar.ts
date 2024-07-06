ad3690f227de7336fad9bc79dbb34693c34447d2
EscapedSpark <113490727+hzqlingmou@users.noreply.github.com>
2024-07-06 17:03:52 +0800

    test

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,17 +3,6 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
   "/": [
     "",
-    {
-      text: "团队简介",
-      icon: "book",
-      prefix: "/introduction",
-      children: "structure",
-    },
-    {
-      text: "项目",
-      icon: "book",
-      prefix: "/project",
-      children: "structure",
-    },
+
   ],
 });
